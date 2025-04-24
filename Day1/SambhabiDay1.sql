00b8e9983d6bdb241fac38b60860063ff157f0db
Skar-25 <sambhabikar@gmail.com>
2025-04-24 11:50:02 -0500

    Delete Day1/SambhabiDay1.sql

--- Day1/SambhabiDay1.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.categories
-(
-    "categoryID" integer NOT NULL,
-    "categoryName" character varying COLLATE pg_catalog."default" NOT NULL,
-    description character varying COLLATE pg_catalog."default",
-    CONSTRAINT categories_pkey PRIMARY KEY ("categoryID")
-)
